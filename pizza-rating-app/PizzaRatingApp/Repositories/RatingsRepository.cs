73aea3ca9adbbbe702040d7205e78cc91f147703
andersmidtbo <Anders.Midtbo@hapdevtest.onmicrosoft.com>
2022-09-15 19:12:40 +0200

    return container

--- pizza-rating-app/PizzaRatingApp/Repositories/RatingsRepository.cs
+++ pizza-rating-app/PizzaRatingApp/Repositories/RatingsRepository.cs
@@ -2,100 +2,98 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
-
 using Microsoft.Azure.Cosmos;
-using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Caching.Memory;
-
-
-
-using PizzaRatingApp.Configurations;
 using PizzaRatingApp.Models;
 
-namespace PizzaRatingApp.Repositories
+namespace PizzaRatingApp.Repositories;
+
+public class RatingsRepository : IRatingsRepository
 {
-    public class RatingsRepository : IRatingsRepository
+    private static readonly string DatabaseId = "storage";
+
+    private readonly CosmosClient _client;
+    private readonly IMemoryCache _memoryCache;
+
+
+    public RatingsRepository(IMemoryCache cache)
     {
+        _memoryCache = cache;
+    }
 
-        private static readonly string DatabaseId = "storage";
+    public async Task<bool> AddRating(string connectionString, Rating rating)
+    {
+        rating.Id = Guid.NewGuid();
+        rating.Created = DateTime.UtcNow;
 
-        private readonly CosmosClient _client;
-        private readonly IMemoryCache _memoryCache;
+        var c = await GetContainer(connectionString);
+        var res = await c.CreateItemAsync(rating);
+        return res.StatusCode == HttpStatusCode.Created;
+    }
 
+    public async Task<List<Rating>> GetRatings(string connectionString, int? min = null)
+    {
+        var c = await GetContainer(connectionString);
 
-        public RatingsRepository(IMemoryCache cache)
+        var queryString = min == null
+            ? "SELECT * FROM ratings"
+            : $"SELECT * FROM ratings r WHERE r.created >= '{DateTime.UtcNow.AddMinutes(-(double)min).ToString("yyyy-MM-ddTHH:mm:ss.fffffffZ")}'";
+
+        List<Rating> ratings = new();
+
+        using (var resultSetIterator = c.GetItemQueryIterator<Rating>(new QueryDefinition(queryString)))
         {
-            _memoryCache = cache;
-        }
-
-        public async Task<bool> AddRating(string connectionString, Rating rating)
-        {
-            rating.Id = Guid.NewGuid();
-            rating.Created = DateTime.UtcNow;
-
-            Container c = await GetContainer(connectionString);
-            ItemResponse<Rating> res = await c.CreateItemAsync(rating);
-            return res.StatusCode == HttpStatusCode.Created;
-        }
-
-        public async Task<List<Rating>> GetRatings(string connectionString, int? min = null)
-        {
-            Container c = await GetContainer(connectionString);
-
-            string queryString = min == null ? "SELECT * FROM ratings" : $"SELECT * FROM ratings r WHERE r.created >= '{DateTime.UtcNow.AddMinutes(-(double)min).ToString("yyyy-MM-ddTHH:mm:ss.fffffffZ")}'";
-
-            List<Rating> ratings = new();
-
-            using (FeedIterator<Rating> resultSetIterator = c.GetItemQueryIterator<Rating>(new QueryDefinition(queryString)))
+            while (resultSetIterator.HasMoreResults)
             {
-                while (resultSetIterator.HasMoreResults)
-                {
-                    FeedResponse<Rating> response = await resultSetIterator.ReadNextAsync();
-                    ratings.AddRange(response);
-                    if (response.Diagnostics != null)
-                    {
-                        Console.WriteLine($"\nQueryWithSqlParameters Diagnostics: {response.Diagnostics.ToString()}");
-                    }
-                }
+                var response = await resultSetIterator.ReadNextAsync();
+                ratings.AddRange(response);
+                if (response.Diagnostics != null)
+                    Console.WriteLine($"\nQueryWithSqlParameters Diagnostics: {response.Diagnostics}");
             }
-
-            return ratings;
         }
 
-        public async Task<List<Rating>> GetRatingsForPizza(string connectionString, int pizzaId)
+        return ratings;
+    }
+
+    public async Task<List<Rating>> GetRatingsForPizza(string connectionString, int pizzaId)
+    {
+        var c = await GetContainer(connectionString);
+
+        var query = new QueryDefinition($"SELECT * FROM ratings r WHERE r.pizzaId = {pizzaId}");
+
+        List<Rating> ratings = new();
+
+        using (var resultSetIterator = c.GetItemQueryIterator<Rating>(query))
         {
-            Container c = await GetContainer(connectionString);
-
-            QueryDefinition query = new QueryDefinition($"SELECT * FROM ratings r WHERE r.pizzaId = {pizzaId}");
-
-            List<Rating> ratings = new();
-
-            using (FeedIterator<Rating> resultSetIterator = c.GetItemQueryIterator<Rating>(query))
+            while (resultSetIterator.HasMoreResults)
             {
-                while (resultSetIterator.HasMoreResults)
-                {
-                    FeedResponse<Rating> response = await resultSetIterator.ReadNextAsync();
-                    ratings.AddRange(response);
-                }
+                var response = await resultSetIterator.ReadNextAsync();
+                ratings.AddRange(response);
             }
-
-            return ratings;
         }
 
-        private async Task<Container> GetContainer(string connectionString)
-        {                 
-            var container = await _memoryCache.GetOrCreateAsync(connectionString, async cacheEntry => 
-            {
+        return ratings;
+    }
+
+    private async Task<Container> GetContainer(string connectionString)
+    {
+        var container = await _memoryCache.GetOrCreateAsync(connectionString, async cacheEntry =>
+        {
             var _client = new CosmosClient(connectionString,
-                new CosmosClientOptions { SerializerOptions = new CosmosSerializationOptions { PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase } });
-       
-                Database database = await _client.CreateDatabaseIfNotExistsAsync(DatabaseId);
+                new CosmosClientOptions
+                {
+                    SerializerOptions = new CosmosSerializationOptions
+                        { PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase }
+                });
 
-            ContainerProperties containerProperties = new ContainerProperties(id: $"ratings", partitionKeyPath: "/pizzaId");
+            Database database = await _client.CreateDatabaseIfNotExistsAsync(DatabaseId);
+
+            var containerProperties = new ContainerProperties("ratings", "/pizzaId");
 
             return await database.CreateContainerIfNotExistsAsync(
-                containerProperties: containerProperties);
-                });                    
-        }
+                containerProperties);
+        });
+
+        return container;
     }
 }
